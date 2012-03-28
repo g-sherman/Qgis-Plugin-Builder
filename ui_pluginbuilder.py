4baa4f894c374f4e6dbdf4ce47f720360cc70af6
Gary Sherman <gsherman@geoapt.com>
2012-03-28 07:31:54 -0800

    Update version number in the title bar

--- ui_pluginbuilder.py
+++ ui_pluginbuilder.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui_pluginbuilder.ui'
 #
-# Created: Wed Dec 14 07:59:25 2011
-#      by: PyQt4 UI code generator 4.8.6
+# Created: Wed Mar 28 07:31:36 2012
+#      by: PyQt4 UI code generator 4.9
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -18,7 +18,6 @@ class Ui_PluginBuilder(object):
     def setupUi(self, PluginBuilder):
         PluginBuilder.setObjectName(_fromUtf8("PluginBuilder"))
         PluginBuilder.resize(893, 601)
-        PluginBuilder.setWindowTitle(QtGui.QApplication.translate("PluginBuilder", "QGIS Plugin Builder 1.8.2", None, QtGui.QApplication.UnicodeUTF8))
         PluginBuilder.setSizeGripEnabled(False)
         self.gridLayout = QtGui.QGridLayout(PluginBuilder)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
@@ -44,11 +43,6 @@ class Ui_PluginBuilder(object):
         self.line.setObjectName(_fromUtf8("line"))
         self.gridLayout.addWidget(self.line, 0, 1, 15, 2)
         self.label_10 = QtGui.QLabel(PluginBuilder)
-        self.label_10.setText(QtGui.QApplication.translate("PluginBuilder", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600;\">QGIS Plugin Builder</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
         self.label_10.setAlignment(QtCore.Qt.AlignCenter)
         self.label_10.setObjectName(_fromUtf8("label_10"))
         self.gridLayout.addWidget(self.label_10, 0, 3, 1, 1)
@@ -57,7 +51,6 @@ class Ui_PluginBuilder(object):
         self.label = QtGui.QLabel(PluginBuilder)
         self.label.setMinimumSize(QtCore.QSize(150, 0))
         self.label.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label.setText(QtGui.QApplication.translate("PluginBuilder", "Class name", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label.setObjectName(_fromUtf8("label"))
         self.horizontalLayout.addWidget(self.label)
@@ -73,7 +66,6 @@ class Ui_PluginBuilder(object):
         self.label_2 = QtGui.QLabel(PluginBuilder)
         self.label_2.setMinimumSize(QtCore.QSize(150, 0))
         self.label_2.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_2.setText(QtGui.QApplication.translate("PluginBuilder", "Descriptive title", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.horizontalLayout_2.addWidget(self.label_2)
@@ -89,7 +81,6 @@ class Ui_PluginBuilder(object):
         self.label_3 = QtGui.QLabel(PluginBuilder)
         self.label_3.setMinimumSize(QtCore.QSize(150, 0))
         self.label_3.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_3.setText(QtGui.QApplication.translate("PluginBuilder", "Description", None, QtGui.QApplication.UnicodeUTF8))
         self.label_3.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.horizontalLayout_3.addWidget(self.label_3)
@@ -105,14 +96,12 @@ class Ui_PluginBuilder(object):
         self.label_4 = QtGui.QLabel(PluginBuilder)
         self.label_4.setMinimumSize(QtCore.QSize(150, 0))
         self.label_4.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_4.setText(QtGui.QApplication.translate("PluginBuilder", "Version number", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_4.setObjectName(_fromUtf8("label_4"))
         self.horizontalLayout_4.addWidget(self.label_4)
         self.lineEdit_version_no = QtGui.QLineEdit(PluginBuilder)
         self.lineEdit_version_no.setMinimumSize(QtCore.QSize(60, 0))
         self.lineEdit_version_no.setMaximumSize(QtCore.QSize(60, 16777215))
-        self.lineEdit_version_no.setText(QtGui.QApplication.translate("PluginBuilder", "0.1", None, QtGui.QApplication.UnicodeUTF8))
         self.lineEdit_version_no.setObjectName(_fromUtf8("lineEdit_version_no"))
         self.horizontalLayout_4.addWidget(self.lineEdit_version_no)
         self.gridLayout.addLayout(self.horizontalLayout_4, 4, 2, 1, 2)
@@ -121,14 +110,12 @@ class Ui_PluginBuilder(object):
         self.label_5 = QtGui.QLabel(PluginBuilder)
         self.label_5.setMinimumSize(QtCore.QSize(150, 0))
         self.label_5.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_5.setText(QtGui.QApplication.translate("PluginBuilder", "Minimum QGIS version", None, QtGui.QApplication.UnicodeUTF8))
         self.label_5.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_5.setObjectName(_fromUtf8("label_5"))
         self.horizontalLayout_5.addWidget(self.label_5)
         self.lineEdit_min_version_no = QtGui.QLineEdit(PluginBuilder)
         self.lineEdit_min_version_no.setMinimumSize(QtCore.QSize(60, 0))
         self.lineEdit_min_version_no.setMaximumSize(QtCore.QSize(60, 16777215))
-        self.lineEdit_min_version_no.setText(QtGui.QApplication.translate("PluginBuilder", "1.0", None, QtGui.QApplication.UnicodeUTF8))
         self.lineEdit_min_version_no.setObjectName(_fromUtf8("lineEdit_min_version_no"))
         self.horizontalLayout_5.addWidget(self.lineEdit_min_version_no)
         self.gridLayout.addLayout(self.horizontalLayout_5, 5, 2, 1, 2)
@@ -137,7 +124,6 @@ class Ui_PluginBuilder(object):
         self.label_6 = QtGui.QLabel(PluginBuilder)
         self.label_6.setMinimumSize(QtCore.QSize(150, 0))
         self.label_6.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_6.setText(QtGui.QApplication.translate("PluginBuilder", "Text for the menu item", None, QtGui.QApplication.UnicodeUTF8))
         self.label_6.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_6.setObjectName(_fromUtf8("label_6"))
         self.horizontalLayout_6.addWidget(self.label_6)
@@ -153,7 +139,6 @@ class Ui_PluginBuilder(object):
         self.label_7 = QtGui.QLabel(PluginBuilder)
         self.label_7.setMinimumSize(QtCore.QSize(150, 0))
         self.label_7.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_7.setText(QtGui.QApplication.translate("PluginBuilder", "Author/Company", None, QtGui.QApplication.UnicodeUTF8))
         self.label_7.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_7.setObjectName(_fromUtf8("label_7"))
         self.horizontalLayout_7.addWidget(self.label_7)
@@ -169,7 +154,6 @@ class Ui_PluginBuilder(object):
         self.label_8 = QtGui.QLabel(PluginBuilder)
         self.label_8.setMinimumSize(QtCore.QSize(150, 0))
         self.label_8.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_8.setText(QtGui.QApplication.translate("PluginBuilder", "Email address", None, QtGui.QApplication.UnicodeUTF8))
         self.label_8.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_8.setObjectName(_fromUtf8("label_8"))
         self.horizontalLayout_8.addWidget(self.label_8)
@@ -181,7 +165,6 @@ class Ui_PluginBuilder(object):
         self.horizontalLayout_8.addWidget(self.lineEdit_email_address)
         self.gridLayout.addLayout(self.horizontalLayout_8, 8, 2, 1, 2)
         self.label_14 = QtGui.QLabel(PluginBuilder)
-        self.label_14.setText(QtGui.QApplication.translate("PluginBuilder", "Optional Items", None, QtGui.QApplication.UnicodeUTF8))
         self.label_14.setAlignment(QtCore.Qt.AlignCenter)
         self.label_14.setObjectName(_fromUtf8("label_14"))
         self.gridLayout.addWidget(self.label_14, 9, 3, 1, 1)
@@ -190,7 +173,6 @@ class Ui_PluginBuilder(object):
         self.label_9 = QtGui.QLabel(PluginBuilder)
         self.label_9.setMinimumSize(QtCore.QSize(150, 0))
         self.label_9.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_9.setText(QtGui.QApplication.translate("PluginBuilder", "Bug tracker", None, QtGui.QApplication.UnicodeUTF8))
         self.label_9.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_9.setObjectName(_fromUtf8("label_9"))
         self.horizontalLayout_9.addWidget(self.label_9)
@@ -205,7 +187,6 @@ class Ui_PluginBuilder(object):
         self.label_11 = QtGui.QLabel(PluginBuilder)
         self.label_11.setMinimumSize(QtCore.QSize(150, 0))
         self.label_11.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_11.setText(QtGui.QApplication.translate("PluginBuilder", "Home page", None, QtGui.QApplication.UnicodeUTF8))
         self.label_11.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_11.setObjectName(_fromUtf8("label_11"))
         self.horizontalLayout_10.addWidget(self.label_11)
@@ -220,7 +201,6 @@ class Ui_PluginBuilder(object):
         self.label_13 = QtGui.QLabel(PluginBuilder)
         self.label_13.setMinimumSize(QtCore.QSize(150, 0))
         self.label_13.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_13.setText(QtGui.QApplication.translate("PluginBuilder", "Repository", None, QtGui.QApplication.UnicodeUTF8))
         self.label_13.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_13.setObjectName(_fromUtf8("label_13"))
         self.horizontalLayout_12.addWidget(self.label_13)
@@ -235,7 +215,6 @@ class Ui_PluginBuilder(object):
         self.label_12 = QtGui.QLabel(PluginBuilder)
         self.label_12.setMinimumSize(QtCore.QSize(150, 0))
         self.label_12.setMaximumSize(QtCore.QSize(150, 16777215))
-        self.label_12.setText(QtGui.QApplication.translate("PluginBuilder", "Tags", None, QtGui.QApplication.UnicodeUTF8))
         self.label_12.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_12.setObjectName(_fromUtf8("label_12"))
         self.horizontalLayout_11.addWidget(self.label_12)
@@ -246,7 +225,6 @@ class Ui_PluginBuilder(object):
         self.horizontalLayout_11.addWidget(self.lineEdit_tags)
         self.gridLayout.addLayout(self.horizontalLayout_11, 13, 3, 1, 1)
         self.checkBox_experimental = QtGui.QCheckBox(PluginBuilder)
-        self.checkBox_experimental.setText(QtGui.QApplication.translate("PluginBuilder", "Flag the plugin as experimental", None, QtGui.QApplication.UnicodeUTF8))
         self.checkBox_experimental.setObjectName(_fromUtf8("checkBox_experimental"))
         self.gridLayout.addWidget(self.checkBox_experimental, 14, 3, 1, 1)
         self.line_2 = QtGui.QFrame(PluginBuilder)
@@ -265,6 +243,27 @@ class Ui_PluginBuilder(object):
         QtCore.QMetaObject.connectSlotsByName(PluginBuilder)
 
     def retranslateUi(self, PluginBuilder):
-        pass
+        PluginBuilder.setWindowTitle(QtGui.QApplication.translate("PluginBuilder", "QGIS Plugin Builder 1.8.3", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_10.setText(QtGui.QApplication.translate("PluginBuilder", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600;\">QGIS Plugin Builder</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("PluginBuilder", "Class name", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("PluginBuilder", "Descriptive title", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("PluginBuilder", "Description", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("PluginBuilder", "Version number", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit_version_no.setText(QtGui.QApplication.translate("PluginBuilder", "0.1", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_5.setText(QtGui.QApplication.translate("PluginBuilder", "Minimum QGIS version", None, QtGui.QApplication.UnicodeUTF8))
+        self.lineEdit_min_version_no.setText(QtGui.QApplication.translate("PluginBuilder", "1.0", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_6.setText(QtGui.QApplication.translate("PluginBuilder", "Text for the menu item", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_7.setText(QtGui.QApplication.translate("PluginBuilder", "Author/Company", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_8.setText(QtGui.QApplication.translate("PluginBuilder", "Email address", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_14.setText(QtGui.QApplication.translate("PluginBuilder", "Optional Items", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_9.setText(QtGui.QApplication.translate("PluginBuilder", "Bug tracker", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_11.setText(QtGui.QApplication.translate("PluginBuilder", "Home page", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_13.setText(QtGui.QApplication.translate("PluginBuilder", "Repository", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_12.setText(QtGui.QApplication.translate("PluginBuilder", "Tags", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBox_experimental.setText(QtGui.QApplication.translate("PluginBuilder", "Flag the plugin as experimental", None, QtGui.QApplication.UnicodeUTF8))
 
 from PyQt4 import QtWebKit
