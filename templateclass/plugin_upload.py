dc61703ff56f9311934f077ba0880b7ad9c2ea16
Tim Sutton <tim@linfiniti.com>
2014-04-09 00:03:16 +0200

    PEP8 and formatting, docstring improvements

--- templateclass/plugin_upload.py
+++ templateclass/plugin_upload.py
@@ -3,31 +3,43 @@
 #
 # Author: A. Pasotti, V. Picavet
 
-import xmlrpclib, sys, os
+import sys
 import getpass
+import xmlrpclib
 from optparse import OptionParser
 
 # Configuration
-PROTOCOL='http'
-SERVER='plugins.qgis.org'
-PORT='80'
-ENDPOINT='/plugins/RPC2/'
-VERBOSE=False
+PROTOCOL = 'http'
+SERVER = 'plugins.qgis.org'
+PORT = '80'
+ENDPOINT = '/plugins/RPC2/'
+VERBOSE = False
 
 def main(options, args):
-    address = "%s://%s:%s@%s:%s%s" % (PROTOCOL, options.username, options.password,
-            options.server, options.port, ENDPOINT)
-    print "Connecting to: %s" % hidepassword(address)
-    
+    """Main entry point.
+
+    :param options: Command line options.
+    :param args: Command line arguments.
+    """
+    address = "%s://%s:%s@%s:%s%s" % (
+        PROTOCOL,
+        options.username,
+        options.password,
+        options.server,
+        options.port,
+        ENDPOINT)
+    print "Connecting to: %s" % hide_password(address)
+
     server = xmlrpclib.ServerProxy(address, verbose=VERBOSE)
-    
+
     try:
-        plugin_id, version_id = server.plugin.upload(xmlrpclib.Binary(open(args[0]).read()))
+        plugin_id, version_id = server.plugin.upload(
+            xmlrpclib.Binary(open(args[0]).read()))
         print "Plugin ID: %s" % plugin_id
         print "Version ID: %s" % version_id
     except xmlrpclib.ProtocolError, err:
         print "A protocol error occurred"
-        print "URL: %s" % hidepassword(err.url, 0)
+        print "URL: %s" % hide_password(err.url, 0)
         print "HTTP/HTTPS headers: %s" % err.headers
         print "Error code: %d" % err.errcode
         print "Error message: %s" % err.errmsg
@@ -36,24 +48,38 @@ def main(options, args):
         print "Fault code: %d" % err.faultCode
         print "Fault string: %s" % err.faultString
 
-def hidepassword(url, start = 6):
-    """Returns the http url with password part replaced with '*'."""
-    passdeb = url.find(':', start) + 1
-    passend = url.find('@')
-    return "%s%s%s" % (url[:passdeb], '*' * (passend - passdeb), url[passend:])
+def hide_password(url, start = 6):
+    """Returns the http url with password part replaced with '*'.
+
+    :param url: URL to upload the plugin to.
+    :type url: str
+
+    :param start: Position of start of password.
+    :type start: int
+    """
+    start_position = url.find(':', start) + 1
+    end_position = url.find('@')
+    return "%s%s%s" % (
+        url[:start_position],
+        '*' * (end_position - start_position),
+        url[end_position:])
 
 
 if __name__ == "__main__":
     parser = OptionParser(usage="%prog [options] plugin.zip")
-    parser.add_option("-w", "--password", dest="password",
-            help="Password for plugin site", metavar="******")
-    parser.add_option("-u", "--username", dest="username",
-            help="Username of plugin site", metavar="user")
-    parser.add_option("-p", "--port", dest="port",
-            help="Server port to connect to", metavar="80")
-    parser.add_option("-s", "--server", dest="server",
-            help="Specify server name", metavar="plugins.qgis.org")
-    (options, args) = parser.parse_args()
+    parser.add_option(
+        "-w", "--password", dest="password",
+        help="Password for plugin site", metavar="******")
+    parser.add_option(
+        "-u", "--username", dest="username",
+        help="Username of plugin site", metavar="user")
+    parser.add_option(
+        "-p", "--port", dest="port",
+        help="Server port to connect to", metavar="80")
+    parser.add_option(
+        "-s", "--server", dest="server",
+        help="Specify server name", metavar="plugins.qgis.org")
+    options, args = parser.parse_args()
     if len(args) != 1:
         print "Please specify zip file.\n"
         parser.print_help()
