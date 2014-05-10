9dd0abee3b567abf4e4fb1a27a75b2af2467f264
Tim Sutton <tim@linfiniti.com>
2014-05-10 23:00:39 +0200

    Wrap menu item in translation request. Removed reference to inasafe in translation update script.

--- plugin_template/scripts/update-strings.sh
+++ plugin_template/scripts/update-strings.sh
@@ -45,7 +45,7 @@ then
   echo "Please provide translations by editing the translation files below:"
   for LOCALE in $LOCALES
   do
-    echo "safe_qgis/i18n/inasafe_"$LOCALE".ts"
+    echo "i18n/"$LOCALE".ts"
     # Note we don't use pylupdate with qt .pro file approach as it is flakey
     # about what is made available.
     pylupdate4 -noobsolete $PYTHON_FILES -ts i18n/${LOCALE}.ts
