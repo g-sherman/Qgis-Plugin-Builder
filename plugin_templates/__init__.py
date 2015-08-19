b215994576bff9194a545ab7480888bae2a160e4
Worth Lutz <worthlutz@mindspring.com>
2015-08-19 12:50:45 -0400

    add dockwidget plugin template

--- plugin_templates/__init__.py
+++ plugin_templates/__init__.py
@@ -1,7 +1,9 @@
 def templates():
     from toolbutton_with_dialog.plugin_template import ToolbuttonWithDialogPluginTemplate
+    from toolbutton_with_dockwidget.plugin_template import ToolbuttonWithDockWidgetPluginTemplate
     from processing_provider.plugin_template import ProcessingProviderPluginTemplate
     return [
         ToolbuttonWithDialogPluginTemplate(),
+        ToolbuttonWithDockWidgetPluginTemplate(),
         ProcessingProviderPluginTemplate()
     ]
